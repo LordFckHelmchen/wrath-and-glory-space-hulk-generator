7886e5155d160a34db494137bc8ee904fa118529
D. J. Mack <dj_mack@gmx.de>
2022-06-09 18:55:52 +0200

    tests: Add test for boolean function to check if an object is parsable to a LayoutEngine

--- tests/test_layout_engine.py
+++ tests/test_layout_engine.py
@@ -1,22 +1,36 @@
 import unittest
-
-from pydantic import conint
+from enum import Enum
 
 from src.wrath_and_glory_space_hulk_generator.layout_engine import LayoutEngine
 
 
-def get_layout_engine_from_index(index: conint(ge=0, le=len(LayoutEngine))) -> LayoutEngine:
-    return next(e for i, e in enumerate(LayoutEngine) if i == index)
+class InvalidLayoutEngine(Enum):
+    ONE = "001"
+    ELEVEN = 0o11
+    SIXTY_SIX = None
 
 
 class TestLayoutEngine(unittest.TestCase):
+    def test_is_parsable_to_layout_engine_with_members_expect_always_true(self) -> None:
+        for member_type, member in {"Member": LayoutEngine.FDP,
+                                    "Member name": LayoutEngine.CIRCO.name,
+                                    "Member value": LayoutEngine.OSAGE.value}.items():
+            with self.subTest(i=member_type):
+                self.assertTrue(LayoutEngine.is_parsable_to_layout_engine(member))
 
-    def test_index_with_member_attributes_expect_always_correct_index_returned(self):
-        for expected_index, (engine, access_fun) in enumerate(
-                zip(LayoutEngine, [lambda x: x, lambda x: x.value, lambda x: x.name])):
-            with self.subTest(i=f"{access_fun(engine)}"):
-                observed_index = LayoutEngine.index(access_fun(engine))
-                self.assertEqual(observed_index, expected_index, f"Expected {expected_index} for {access_fun(engine)}")
+    def test_is_parsable_to_layout_engine_with_non_members_and_types_expect_always_false(self) -> None:
+        for non_member in [InvalidLayoutEngine.ONE, 2, "3", {"4": 5.0}]:
+            with self.subTest(i=type(non_member)):
+                self.assertFalse(LayoutEngine.is_parsable_to_layout_engine(non_member))
+
+    def test_index_with_all_members_expect_always_correct_index_returned(self) -> None:
+        for expected_index, engine in enumerate(LayoutEngine):
+            for access_fun in [lambda x: x, lambda x: x.value, lambda x: x.name]:
+                with self.subTest(i=f"{access_fun(engine)}"):
+                    member = access_fun(engine)
+                    self.assertEqual(expected_index,
+                                     LayoutEngine.index(member),
+                                     f"Expected '{expected_index}' for '{member}' of type {type(member)})")
 
 
 if __name__ == '__main__':
