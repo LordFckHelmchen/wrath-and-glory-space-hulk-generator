e5c6d3863f0de19ee1994779eace58370a043971
D. J. Mack <dj_mack@gmx.de>
2022-10-14 23:41:15 +0200

    tests(generator): Fix failing unittest
    Copy-by-reference in dicts was the problem. Deepcopy solved it.

--- src/generator/random_table_event_collection.py
+++ src/generator/random_table_event_collection.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from typing import Generator
 from typing import List
 from typing import Optional
@@ -32,21 +33,27 @@ class RandomTableEventCollection(BaseModel):
             else:
                 event_counts[event.name] += 1
 
-        duplicated_event_ids = {event.name: 0 for event in events if event_counts[event.name] > 1}
+        duplicated_event_ids = {event.name: 0 for event in events if
+                                event_counts[event.name] > 1}
+        new_events = []
         for event in events:
+            new_event = deepcopy(event)
             if event.name in duplicated_event_ids:
                 duplicated_event_ids[event.name] += 1
-                event.name = f"{event.name} No.{duplicated_event_ids[event.name]}"
+                new_event.name = f"{event.name} No.{duplicated_event_ids[event.name]}"
+            new_events.append(new_event)
 
-        return sorted(events, key=lambda x: x.name)
+        return sorted(new_events, key=lambda x: x.name)
 
     def __setattr__(self, key: str, value):
         _ = getattr(self, key)  # Raise AttributeError if not present
 
         if key == "events":
             if not isinstance(value, type(self.events)) or not all(
-                    type(e) in self.__annotations__["events"].__args__ for e in value):
-                raise EventTypeError(f"'events' must be of type '{type(self.events)}', was '{type(value)}'")
+                    type(e) in self.__annotations__["events"].__args__ for e in
+                    value):
+                raise EventTypeError(
+                    f"'events' must be of type '{type(self.events)}', was '{type(value)}'")
             elif len(value) not in self.event_count_constraint:
                 raise EventCountOutOfRangeError(f"Event count out of range. "
                                                 f"'events' must have a number of elements within "
@@ -54,9 +61,10 @@ class RandomTableEventCollection(BaseModel):
 
             value = self.assure_events_are_sorted_and_unique(value)
         elif len(self.events) not in value:  # key == "event_count_constraint":
-            raise ValueError(f"Setting an event count constraint that would invalidate the current events. "
-                             f"Number of current events {len(self.events)}, 'event_count_constraint': {value}. "
-                             f"If this occurred during __init__, provide matching 'events'")
+            raise ValueError(
+                f"Setting an event count constraint that would invalidate the current events. "
+                f"Number of current events {len(self.events)}, 'event_count_constraint': {value}. "
+                f"If this occurred during __init__, provide matching 'events'")
 
         object.__setattr__(self, key, value)
 
@@ -67,15 +75,18 @@ class RandomTableEventCollection(BaseModel):
         for event in self.events:
             yield event
 
-    def as_markdown(self, header: Optional[str] = None, header_level: PositiveInt = 1) -> str:
-        self_as_string = [f"{'#' * header_level} {header} (n={len(self)})\n"] if header else []
+    def as_markdown(self, header: Optional[str] = None,
+                    header_level: PositiveInt = 1) -> str:
+        self_as_string = [
+            f"{'#' * header_level} {header} (n={len(self)})\n"] if header else []
 
         if len(self) == 0:
             self_as_string.append("None\n")
         elif len(self) == 1:
             self_as_string.append(f"{self.events[0].name_with_description}\n")
         else:
-            self_as_string.append("".join([f"{i}. {event.name_with_description}\n"
-                                           for i, event in enumerate(self.events)]))
+            self_as_string.append(
+                "".join([f"{i}. {event.name_with_description}\n"
+                         for i, event in enumerate(self.events)]))
 
         return "\n".join(self_as_string)
