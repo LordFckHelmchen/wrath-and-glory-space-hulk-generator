dbfa22e8e8c64d4195be05a92aef94a43b3209ad
D. J. Mack <dj_mack@gmx.de>
2023-03-11 15:18:16 +0100

    tests: Fix app test not running within venv

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,7 +11,6 @@ import toml
 
 
 class TestRandomTable(unittest.TestCase):
-
     def test_run_app_curl_main_page_expect_same_result_as_always(self):
         # ARRANGE
         test_dir = Path(__file__).parent
@@ -20,16 +19,19 @@ class TestRandomTable(unittest.TestCase):
         streamlit_config = toml.load(working_dir / ".streamlit" / "config.toml")
         app_url = f"http://localhost:{streamlit_config['server']['port']}"
         expected_response = (
-                test_dir / "assets" / "streamlit_http_get_response.html").read_text()
+            test_dir / "assets" / "streamlit_http_get_response.html"
+        ).read_text()
+        poetry = which("poetry")
+        curl = which("curl")
 
         # ACT
         with Popen(
-                [which("poetry"), "run", which("streamlit"), "run", app_name,
-                 "--server.headless",
-                 "true"], cwd=working_dir, stdout=PIPE) as app:
+            [poetry, "run", "streamlit", "run", app_name, "--server.headless", "true"],
+            cwd=working_dir,
+            stdout=PIPE,
+        ) as app:
             sleep(2)  # Wait until started.
-            response = run([which("curl"), app_url], capture_output=True,
-                           text=True)
+            response = run([curl, app_url], capture_output=True, text=True)
             app.terminate()
 
         # ASSERT
@@ -37,5 +39,5 @@ class TestRandomTable(unittest.TestCase):
         self.assertEqual(response.stdout, expected_response)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
