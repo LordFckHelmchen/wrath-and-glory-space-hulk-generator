679d9cf5977c17d3533e110b96a6c9b359a4ada7
D. J. Mack <dj_mack@gmx.de>
2023-03-12 16:24:19 +0100

    test: Add retry attempt for app test to circumvent initial run problems

--- tests/test_app.py
+++ tests/test_app.py
@@ -20,16 +20,15 @@ class TestApp(unittest.TestCase):
         expected_html_file = test_dir / "assets" / "streamlit_http_get_response.html"
         expected_html = expected_html_file.read_text()
         poetry = which("poetry")
+        streamlit_args = ["--server.headless", "true"]
         curl = which("curl")
+        # Sometime setup takes longer, and we retry to get the connection.
+        curl_args = ["--raw", "--retry-connrefused", "--retry", "2", "--show-error", "--silent"]
 
         # ACT
-        with Popen(
-            [poetry, "run", "streamlit", "run", app_name, "--server.headless", "true"],
-            cwd=working_dir,
-            stdout=PIPE,
-        ) as app:
-            sleep(2)  # Wait until started.
-            response = run([curl, app_url], capture_output=True, text=True)
+        with Popen([poetry, "run", "streamlit", "run", app_name, *streamlit_args], cwd=working_dir, stdout=PIPE) as app:
+            sleep(0.5)  # Give it some time to start, if taken longer, curl will take care of retries.
+            response = run([curl, app_url, *curl_args], capture_output=True, text=True)
             app.terminate()
         actual_html = response.stdout
         (test_dir / f"generated_{expected_html_file.name}").write_text(actual_html)
